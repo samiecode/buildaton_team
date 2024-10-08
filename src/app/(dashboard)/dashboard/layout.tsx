d70b8355633f3bd4c57f37dc1292484f2134e490
SimpleX-T <ntmark2004@gmail.com>
2024-10-08 17:58:32 +0100

    minor changes to the file

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { DashboardLayoutProps } from "../../types";
-import Header from "./header";
-import Sidebar from "./sidebar";
+import Header from "./_components/header";
+import Sidebar from "./_components/sidebar";
+
 const DashBoardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
-  return (
-    <div>
-      <Header />
-      <div className="flex w-full min-h-screen">
-        <Sidebar />
-        <div className="bg-gray-100 min-h-full w-4/5">{children}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main className='grid grid-cols-[250px_1fr] w-full'>
+				<section className='w-[250px] relative'>
+					<Sidebar />
+				</section>
+				<div className='bg-gray-100 h-full w-full p-12'>{children}</div>
+			</main>
+		</div>
+	);
 };
-export default DashBoardLayout
+export default DashBoardLayout;
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,50 +1,54 @@
 import { orbitron } from "@/app/fonts";
-import CourseCard from "./CourseCard";
+import CourseCard from "./_components/CourseCard";
 import { CourseProp } from "@/app/types";
-import Premium from "./Premium";
+import Premium from "./_components/Premium";
 
 const courses: CourseProp[] = [
-  {
-    name: "Course 1",
-    type: "Basics",
-    lessons: 3,
-    duration: 30,
-    progress: 87,
-  },
-  {
-    name: "Course 2",
-    type: "Wallets",
-    lessons: 4,
-    duration: 40,
-    progress: 32,
-  },
-  {
-    name: "Course 3",
-    type: "Transactions",
-    lessons: 5,
-    duration: 40,
-    progress: 20,
-  },
+	{
+		courseID: "0dh2",
+		name: "Course 1",
+		type: "Basics",
+		lessons: 3,
+		duration: 30,
+		progress: 87,
+	},
+	{
+		courseID: "0de6",
+		name: "Course 2",
+		type: "Wallets",
+		lessons: 4,
+		duration: 40,
+		progress: 32,
+	},
+	{
+		courseID: "w37i",
+		name: "Course 3",
+		type: "Transactions",
+		lessons: 5,
+		duration: 40,
+		progress: 20,
+	},
 ];
 const Dashboard = () => {
-  return (
-    <div className="w-full min-h-full p-10">
-      <h1 className={`${orbitron.className} font-semibold text-2xl`}>
-        Active Courses
-      </h1>
-      <div className="flex justify-start items-center gap-4">
-        {courses.map((course) => (
-          <CourseCard
-            lessons={course.lessons}
-            name={course.name}
-            type={course.type}
-            duration={course.duration}
-            progress={course.progress}
-          />
-        ))}
-        <Premium />
-      </div>
-    </div>
-  );
+	return (
+		<div className='w-full min-h-full p-10'>
+			<h1 className={`${orbitron.className} font-semibold text-2xl`}>
+				Active Courses
+			</h1>
+			<div className='flex justify-start items-center gap-4'>
+				{courses.map((course, index) => (
+					<CourseCard
+						key={course.courseID || index}
+						lessons={course.lessons}
+						name={course.name}
+						type={course.type}
+						duration={course.duration}
+						progress={course.progress}
+					/>
+				))}
+				<Premium />
+			</div>
+		</div>
+	);
 };
 export default Dashboard;
